1f12dd0c523f727a00cd1c863505de1e211e05aa
OtterSpy <mr.roma.nelson@gmail.com>
2021-10-12 17:48:13 +0300

    Не законченная пагинация

--- app/src/main/java/com/example/popularfilmsapp/domain/model/MovieItem.kt
+++ app/src/main/java/com/example/popularfilmsapp/domain/model/MovieItem.kt
@@ -7,13 +7,17 @@ data class MovieItem(
     @SerializedName("backdrop_path")
     val backdropPath: String,
     val id: Int,
+    @SerializedName("original_language")
     val originalLanguage: String,
+    @SerializedName("original_title")
     val originalTitle: String,
     val overview: String,
     val popularity: String,
     @SerializedName("poster_path")
     val posterPath: String,
+    @SerializedName("release_date")
     val releaseDate: String,
+    @SerializedName("vote_average")
     val voteAverage: Double,
     val title: String
 )
